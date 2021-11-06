518e61149c2590e72a496f7b7550c55c2955a861
oppara <oppara@oppara.tv>
2021-11-07 01:01:01 +0900

    :sparkles: (aws) add cdk stepfunctions-events

--- aws/cdk/stepfunctions-events/README.md
+++ aws/cdk/stepfunctions-events/README.md
@@ -4,6 +4,8 @@ This is a blank project for TypeScript development with CDK.
 
 The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
+[Amazon EventBridgeルールでAWS Step Functionsステートマシンを定期実行する | DevelopersIO](https://dev.classmethod.jp/articles/run-the-aws-step-functions-state-machine-periodically-with-amazon-eventbridge-rules/)
+
 ## Useful commands
 
  * `npm run build`   compile typescript to js
@@ -12,3 +14,4 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
--- aws/cdk/stepfunctions-events/lib/stepfunctions-events-stack.ts
+++ aws/cdk/stepfunctions-events/lib/stepfunctions-events-stack.ts
@@ -1,9 +1,21 @@
 import * as cdk from '@aws-cdk/core';
+import * as sfn from '@aws-cdk/aws-stepfunctions';
+import * as events from '@aws-cdk/aws-events';
+import * as targets from '@aws-cdk/aws-events-targets';
 
 export class StepfunctionsEventsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const stateMachine = new sfn.StateMachine(this, 'sampleStateMachine', {
+      stateMachineName: 'sampleStateMachine',
+      definition: new sfn.Pass(this, 'pass', {}),
+    });
+
+    new events.Rule(this, 'sampleRule', {
+      ruleName: 'sampleRule',
+      schedule: events.Schedule.rate(cdk.Duration.minutes(5)),
+      targets: [new targets.SfnStateMachine(stateMachine)],
+    });
   }
 }
